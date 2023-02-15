eb9dd7c678fcbc14c841cdeaa8cd1f7a24f03e44
Julião LB <juliaolangassitela@gmail.com>
2023-02-15 18:48:30 +0200

    Create 11_sanitaze_inputs.php
    PHP Sanitize Input.
    
    link: https://www.phptutorial.net/php-tutorial/php-sanitize-input/

---
+++ 11_sanitaze_inputs.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * SECURITY INPUTS
+ */
+if (isset($_POST['submit'])) {
+    //Eng: 1a option: || Pt: 1a opcao
+    // $name = htmlspecialchars($_POST['name']);
+    // $age = htmlspecialchars($_POST['age']);
+    
+    //Eng: 2a option: || Pt: 2a opcao
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    echo $name;
+    echo $age;
+} 
+?>
+
+<!--HTML-->
+<!--Form-->
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+    <div>
+        <label for="name">Name: </label><input type="text" name="name"  required></input>
+    </div>
+    <div>
+        <labelb for="age">age: </label><input type="number" name="age" required></input>
+    </div>
+    <div>
+    <input type="submit" value="submit" name="submit"
+    >
+    </div> 
+</form>
+<!--link-->
+
+<!--end HTML-->
