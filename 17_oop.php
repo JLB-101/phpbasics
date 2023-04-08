c2ed2c42ac5601a76873c9f53c673565d126ced0
Julião LB <104986669+JLB-101@users.noreply.github.com>
2023-04-08 15:20:46 +0200

    Update 17_oop.php
    Object Oriented Programming

---
+++ 17_oop.php
@@ -0,0 +1,19 @@
+<?php
+
+ /*Object Oriented Programming -- */ 
+
+/*
+Eng: From PHP5 onwards you can write PHP in either a procedural or object oriented way . 00P consists of classes that can hold " properties " and " methods " . Objects can be created from classes .
+
+/*Programação Orientada a Objetos -- */ 
+
+/* Do PHP5 em diante, você pode escrever PHP de maneira procedural ou orientada a objetos.  00P consiste em classes que podem conter "propriedades" e "métodos".  Objetos podem ser criados a partir de classes.*/
+
+*/
+
+
+
+
+
+
+?>
