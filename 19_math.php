00953f5c65fc8f9f12f18724415652a70ae4062b
zishan07cse <zishan07csecuet@gmail.com>
2024-06-02 00:23:28 +0600

    feat:add math function

---
+++ 19_math.php
@@ -0,0 +1,29 @@
+
+<?php 
+/**
+ * Eng: In PHP, a variable starts with the $ sign, followed by the name of the variable:
+ * 
+ * PT:No PHP, uma variável começa com o sinal $, seguido do nome da variável:
+ */
+
+$name = "juliao LB"; //string
+$age = 5; //int
+$height = 10.5; //float
+$ACCOUNTstatus = false; //boll
+
+//output, saída:
+var_dump($name);
+
+
+//PHP Operators
+
+/*Eng: Arithmetic operators
+* Pt: Operadores aritméticos
+*/
+echo 1 + 1; //(+ Sum of $x and $y)
+echo 1 - 1; //(- Difference of $x and $y)
+echo 1 / 1; //(/ Quotient of $x and $y)
+echo 1 * 1; //(* Product of $x and $y)
+echo 4 % 2; //(% Remainder of $x divided by $y)
+echo 4 ** 2; //(** Result of raising $x to the $y'th power)
+?>
